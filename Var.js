359da486a5445906127fb9723ca213bb0e324a3a
shubham <shubham@shubhams-MacBook-Air.local>
2021-05-22 15:39:42 +0530

    first commit

---
+++ Var.js
@@ -0,0 +1,60 @@
+
+
+// // {variables}
+
+// var myname = 'shubham'
+
+// console.log(myname)
+// // const myname = 'shubhamconst' cannot reasign var with diff const
+
+
+
+// const myfirstname = 'shubham';
+// console.log(myfirstname);
+// // const myfirstname = 'shubham1'; cannot change data of const. (it will give you error)
+
+
+
+// // boolean
+
+// var ismale = true;
+// console.log(ismale)
+// console.log(typeof ismale)
+
+// var isfemale = false;
+// console.log(isfemale)
+
+// console.log(typeof isfemale)
+
+// // // null and undifined
+
+// // var babygender = null;
+// // console.log(babygender)  // null gives you nothing it means nothing is asigned to this value
+
+
+
+// var babygender = undefined;
+// console.log(babygender) //gives you the value undefined and that is the difference between null and undefined 
+
+
+// maths api
+
+let x1 = 3;
+let y1 = 5;  //(3, 5)
+
+let x2 = 7;
+let y2 = 8; //(7, 8)
+
+let deltax = Math.pow(x2-x1, 2);
+
+console.log(deltax);
+
+let delta
+
+
+// console.log(deltay);
+
+// let sum = (deltay + deltax);
+// console.log(sum)
+
+
